13d1a0161b0b98f25f0c95f27711865bebc56571
arshadfaislabadi <102044245+arshadfaislabadi@users.noreply.github.com>
2022-03-22 22:54:17 +0500

    reactjsxcom

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+//Simple String
+//const exp = <h2>{10+10}</h2>\
+//VARIABLE ALSO MAKE
+//const name = "muhammad arshad from jaranwala";
+//const el = <h1>Hellow{name}</h1>;
+//function show(name){
+ // return name;
+//}
+let user = {
+  firstname: "muhammad arshad"
+}
+
+const el = <h1>Hellow{user.firstname}</h1>;
+export default el;
---
+++ src/Student.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+//ES6
+class Student extends Component{
+    render(){
+    return <h1>Hellow MY NAME is muhammad arshad</h1>;
+}
+}
+
+export default Student;
+//old method
+//function Student = () =>{return <h1>Hellow my anem is muhamad arshad</h1>}
